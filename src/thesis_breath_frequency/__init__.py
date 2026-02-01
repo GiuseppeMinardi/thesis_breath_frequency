ba8de5241a326b6d27036fe6886eeff2dd169299
GiuseppeMinardi <38011429+GiuseppeMinardi@users.noreply.github.com>
2026-02-01 19:54:23 +0100

    chore: update code structure for better readability and maintainability

--- src/thesis_breath_frequency/__init__.py
+++ src/thesis_breath_frequency/__init__.py
@@ -4,7 +4,14 @@ Expose the most useful symbols from the package so notebooks can import
 directly from the package without modifying `sys.path`.
 """
 
-from .project_configs import ProjectPaths
-from .project_logger import logger
+import logging
+
+from .project_configs import LoggerConfiguration, ProjectPaths
+
+_project_paths = ProjectPaths()
+_logger_config = LoggerConfiguration()
+_loggerdict_config = _logger_config.generate(log_dir=_project_paths.logger_folder)
+logger = logging.getLogger(_logger_config.log_name)
+logger.setLevel(_logger_config.log_level)
 
 __all__ = ["ProjectPaths", "logger"]
